f6cb4c3cfbe482934296aa175b53c2cdc2480b5e
omegatao <omegatao@gmail.com>
2018-05-26 11:25:20 +0800

    updated the GUI  and changed the file position

--- homeControl_main.py
+++ homeControl_main.py
@@ -8,11 +8,16 @@ class HCMainWindow(QMainWindow, Ui_MainWindow):
         super(HCMainWindow,self).__init__(parent)        
         self.setupUi(self)
         
-        self.pushButton_study_light_onfull.clicked.connect(partial(self.mainParser,'study-SC-study_light_onfull'))
-        self.pushButton_study_light_onhalf.clicked.connect(partial(self.mainParser,'study-SC-study_light_on')) 
-        self.pushButton_study_light_onmin.clicked.connect(partial(self.mainParser,'study-SC-study_light_ondim'))
-        self.pushButton_study_light_brighter.clicked.connect(partial(self.mainParser,'study-SC-study_light_brighter'))
-        self.pushButton_study_light_dimmer.clicked.connect(partial(self.mainParser,'study-SC-study_light_dimmer'))        
+        self.pushButton_study_light_onfull.clicked.connect(partial(self.mainParser,'study-SC-study_light_on')) #书房灯全开
+        self.pushButton_study_light_onhalf.clicked.connect(partial(self.mainParser,'study-SC-study_light_onhalf')) #书房灯半开
+        self.pushButton_study_light_onmin.clicked.connect(partial(self.mainParser,'study-SC-study_light_onmin'))#书房灯微光
+        self.pushButton_study_light_brighter.clicked.connect(partial(self.mainParser,'study-SC-study_light_brighter'))#书房灯增亮
+        self.pushButton_study_light_dimmer.clicked.connect(partial(self.mainParser,'study-SC-study_light_dimmer'))#书房灯变暗
+        self.pushButton_study_light_off.clicked.connect(partial(self.mainParser,'study-SC-study_light_off'))#书房灯关闭
+
+        self.pushButton_study_AC_cool26.clicked.connect(partial(self.mainParser,'study-SC-study_AC_cool26'))#书房空调26度制冷
+        self.pushButton_study_AC_off.clicked.connect(partial(self.mainParser,'study-SC-study_AC_off'))#书房空调关
+
 
         
     
